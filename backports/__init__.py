4209dc7d3a63998437636d37664dfe9669c6ccdc
Peter Cock <p.j.a.cock@googlemail.com>
2012-10-28 12:37:56 +0000

    Setup backports namespace

---
+++ backports/__init__.py
@@ -0,0 +1,10 @@
+# This file is part of a backport of 'lzma' included with Python 3.3,
+# exposed under the namespace of backports.lzma following the conventions
+# laid down here: http://pypi.python.org/pypi/backports/1.0
+# Backports homepage: http://bitbucket.org/brandon/backports
+
+# A Python "namespace package" http://www.python.org/dev/peps/pep-0382/
+# This always goes inside of a namespace package's __init__.py
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
